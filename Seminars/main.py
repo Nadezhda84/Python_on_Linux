c6e359d35ffff1abe582aa759d35998f9db1c758
user <nadezdafurman84@gmail.com>
2023-11-06 12:46:01 +0300

    Added Seminar 1

---
+++ Seminars/main.py
@@ -0,0 +1,9 @@
+import subprocess
+
+if __name__ == '__main__':
+    result = subprocess.run("cat /etc/os-release", shell=True, stdout=subprocess.PIPE, encoding='utf-8')
+    out = result.stdout
+    if "22.04.10" in out and "jammy" in out and result.returncode == 0:
+        print("SUCCES")
+    else:
+        print("FAIL")
